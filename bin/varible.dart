efa08339aeebcdbd4188beba18ce563f78be9e60
habibagamal123 <habiba.gamaall23@gmail.com>
2024-09-17 13:26:40 +0300

    varibale

---
+++ bin/varible.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+late   var g1 , g2 , g3 ;
+
+g1 = double.parse(stdin.readLineSync()!);
+g2=double.parse(stdin.readLineSync()!);
+g3=double.parse(stdin.readLineSync()!);
+double avg = (g1+g2 +g3) /3;
+
+
+}
