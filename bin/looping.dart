2380e1c23c5106b0e5aa307c85da0c542c340c2b
habibagamal123 <habiba.gamaall23@gmail.com>
2024-09-17 13:48:34 +0300

    for even

---
+++ bin/looping.dart
@@ -0,0 +1,16 @@
+void main(List<String> arguments) {
+
+  //1 , 5
+  for(int i =1 ; i<=5 ; i++){
+    print(i);
+  }
+// for loop even
+  for(int i =1 ; i<=100 ; i++){
+ if(i%2==0){
+   print(i);
+ }
+  }
+
+
+
+}
