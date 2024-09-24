123e67df7e085596cfff91c0b2fc804941b88b60
habibagamal123 <habiba.gamaall23@gmail.com>
2024-09-24 13:57:51 +0300

    car

--- bin/varible.dart
+++ bin/varible.dart
@@ -1,12 +1,35 @@
-import 'dart:io';
+import 'dart:math';
+void main() {
 
-void main(List<String> arguments) {
-late   var g1 , g2 , g3 ;
+  Car c1 =Car();
+  c1.colorCar="red";
+  c1.model="kia";
+  c1.year= "2021";
+c1.desply();
 
-g1 = double.parse(stdin.readLineSync()!);
-g2=double.parse(stdin.readLineSync()!);
-g3=double.parse(stdin.readLineSync()!);
-double avg = (g1+g2 +g3) /3;
+
+
+  Car c2 = Car();
+c2.model="toyta";
+c2.year="2023";
+c2.colorCar="blue";
+  c2.desply();
+
+
+
 
 
 }
+
+
+class Car {
+  String? colorCar;
+  String ? model;
+  String ?year;
+
+
+  void desply(){
+    print("your car is $colorCar $model $year");
+  }
+}
+
