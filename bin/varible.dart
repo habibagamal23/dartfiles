458eb034b280eaea338f16ef217fbc69b42c22d1
habibagamal123 <habiba.gamaall23@gmail.com>
2024-09-24 14:37:48 +0300

    hospital

--- bin/varible.dart
+++ bin/varible.dart
@@ -1,35 +1,42 @@
 import 'dart:math';
+
 void main() {
+  Hospital shefaa = Hospital();
+  shefaa.name = "sheaffa";
+  shefaa.maxOfNym = 5;
+  shefaa.display();
 
-  Car c1 =Car();
-  c1.colorCar="red";
-  c1.model="kia";
-  c1.year= "2021";
-c1.desply();
-
-
-
-  Car c2 = Car();
-c2.model="toyta";
-c2.year="2023";
-c2.colorCar="blue";
-  c2.desply();
+  Hospital noor = Hospital()..name="noor"..maxOfNym=3..display();
 
 
 
 
 
+/////////////////////////////////////
+  Doctor dr = Doctor();
+  dr.name = "ahmed";
+  dr.sep = "eye";
+  dr.display();
+  Doctor dr2 = Doctor();
+  dr.name = ",o,ed";
+  dr.sep = "eye";
+  dr.display();
 }
 
+class Hospital {
+  String? name;
+  int maxOfNym = 3;
 
-class Car {
-  String? colorCar;
-  String ? model;
-  String ?year;
-
-
-  void desply(){
-    print("your car is $colorCar $model $year");
+  void display() {
+    print("hospital name is $name , Max of patiant is $maxOfNym");
   }
 }
 
+class Doctor {
+  String? name;
+  String? sep;
+
+  void display() {
+    print("doctor  name is $name , sepcialization $sep");
+  }
+}
