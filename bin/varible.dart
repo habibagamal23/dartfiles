fd9aac497fa18e3b96f129892bd9450594bff4fb
habibagamal123 <habiba.gamaall23@gmail.com>
2024-09-24 15:49:40 +0300

    named

--- bin/varible.dart
+++ bin/varible.dart
@@ -1,42 +1,30 @@
 import 'dart:math';
 
 void main() {
-  Hospital shefaa = Hospital();
-  shefaa.name = "sheaffa";
-  shefaa.maxOfNym = 5;
-  shefaa.display();
+Doctor dr = Doctor("Ahmed", "eye");
+Doctor d2 = Doctor.withpayment("noor", "eye", 700);
 
-  Hospital noor = Hospital()..name="noor"..maxOfNym=3..display();
-
-
-
-
-
-/////////////////////////////////////
-  Doctor dr = Doctor();
-  dr.name = "ahmed";
-  dr.sep = "eye";
-  dr.display();
-  Doctor dr2 = Doctor();
-  dr.name = ",o,ed";
-  dr.sep = "eye";
-  dr.display();
 }
 
-class Hospital {
-  String? name;
-  int maxOfNym = 3;
-
-  void display() {
-    print("hospital name is $name , Max of patiant is $maxOfNym");
-  }
-}
 
 class Doctor {
   String? name;
   String? sep;
-
-  void display() {
-    print("doctor  name is $name , sepcialization $sep");
+  int pay= 300;
+  Doctor(String name , String sepp){
+    this.name=name;
+    sep= sepp;
+    print("${this.name} $sep , $pay");
   }
+
+  Doctor.withpayment(String name , String sepp , int paymen){
+    this.name=name;
+    sep= sepp;
+    pay=paymen;
+
+    print("${this.name} $sep , $pay");
+  }
+
+
+
 }
